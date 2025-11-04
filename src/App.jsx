a68e2c9532bbc72dbb9c4616e6b038707e8545e2
Adonis Paniagua <77471071+darkendord@users.noreply.github.com>
2025-11-03 21:36:07 -0600

    Initial repo commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-lvw h-lvh flex justify-center items-center flex-col bg-black">
+        <h1 className="text-white text-5xl">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
